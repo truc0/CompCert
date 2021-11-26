1b4deddf0424f4334829a0e8e3642228adeb2d24
Ling Zhang <ling.zhang@sjtu.edu.cn>
2021-11-26 12:23:18 +0800

    WIP:finding necessary lemmas

--- x86/RealAsmInstrsize.v
+++ x86/RealAsmInstrsize.v
@@ -6,6 +6,7 @@ Require Import Integers.
 Require Import Floats.
 Require Import Asm RealAsm RealAsmRegs.
 
+(*
 Definition transf_program (p: program) : program := p.
 
 Definition match_prog (p :program) (tp:program):= p = tp.
@@ -15,16 +16,13 @@ Lemma transf_program_match:
 Proof.
   intros. reflexivity.
 Qed.
-
+*)
 Section PRESERVATION.
 
 Variables prog: program.
-Variables tprog: program.
-Hypothesis TRANSL: match_prog prog tprog.
 Let ge := Genv.globalenv prog.
-Let tge := Genv.globalenv tprog.
 
-Lemma prog_eq : prog = tprog.
+(*Lemma prog_eq : prog = tprog.
 Proof. auto. Qed.
 
 Lemma genv_eq : ge = tge.
@@ -32,21 +30,65 @@ Proof.
   unfold match_prog in TRANSL. unfold ge.
   unfold tge. congruence.
 Qed.
-
+*)
 Variable instr_size_1 : instruction -> Z.
 Variable instr_size_2 : instruction -> Z.
 
-Fixpoint transf_codeofs (c:code) (ofs1 ofs2 : Z) : option Z :=
-  if (zeq ofs1 0) then Some ofs2 else
+Hypothesis instr_size_1_repr : forall i, 0 <= instr_size_1 i <= Ptrofs.max_unsigned.
+Hypothesis instr_size_1_positive :forall i, 0 < instr_size_1 i.
+Hypothesis instr_size_2_repr : forall i, 0 <= instr_size_2 i <= Ptrofs.max_unsigned.
+Hypothesis instr_size_2_positive :forall i, 0 < instr_size_2 i.
+
+(*
+Fixpoint transf_codeofs' (c:code) (ofs1 ofs2 : Z) : Z :=
+  if (zeq ofs1 0) then ofs2 else
   match c with
-    | nil => None
-    | i :: il => transf_codeofs il (ofs1 - instr_size_1 i) (ofs2+ instr_size_2 i)
+    | nil => 0
+    | i :: il => transf_codeofs' il (ofs1 - instr_size_1 i) (ofs2+ instr_size_2 i)
   end.
+
+Definition transf_codeofs (c:code) (ofs : ptrofs) : ptrofs :=
+  Ptrofs.repr (transf_codeofs' c (Ptrofs.unsigned ofs) 0 ).
+*)
+
+Lemma code_size_positive: forall (is: instruction -> Z) (c:code),
+    (forall i, 0 < is i) -> 0 <= code_size is c .
+Proof.
+  intros. induction c.
+  simpl. lia.
+  simpl. generalize (H a). lia.
+Qed.
+
+Definition match_codeofs (c : code) (ofs1 ofs2 : Z) : Prop :=
+    exists a b, c = a ++ b /\
+    ofs1 = code_size instr_size_1 a /\
+    ofs2 = code_size instr_size_2 a.
+
+Lemma transf_find_instr: forall c ofs1 ofs2 i,
+    match_codeofs c ofs1 ofs2 ->
+    find_instr instr_size_1 ofs1 c = Some i ->
+    find_instr instr_size_2 ofs2 c = Some i.
+Proof.
+  induction c.
+  - intros. inv H. inv H0.
+  - intros. inv H.
+    destruct H1 as (b & H1 & H2 & H3).
+    destruct x.
+    + inv H1. inv H. simpl. inv H0. auto.
+    + inv H1. simpl. destr.
+      generalize (instr_size_2_positive i0).
+      generalize (code_size_positive instr_size_2 x instr_size_2_positive). extlia.
+      simpl in H0. destr_in H0.
+      generalize (instr_size_1_positive i0).
+      generalize (code_size_positive instr_size_1 x instr_size_1_positive). extlia.
+      eapply IHc; eauto. econstructor.
+      exists b. split. eauto. split. lia. lia.
+Qed.
 
 Inductive match_pc : block -> Z -> Z -> Prop :=
   |match_pc_gen: forall b f ofs1 ofs2,
     Genv.find_funct_ptr ge b = Some (Internal f) ->
-    transf_codeofs (fn_code f) ofs1 0 = Some ofs2 ->
+    match_codeofs (fn_code f) ofs1 ofs2 ->
     match_pc b ofs1 ofs2.
 
 Inductive match_value: val -> val -> Prop :=
@@ -54,17 +96,29 @@ Inductive match_value: val -> val -> Prop :=
   |match_long: forall i, match_value (Vlong i) (Vlong i)
   |match_float: forall i, match_value (Vfloat i) (Vfloat i)
   |match_single: forall i, match_value (Vsingle i) (Vsingle i)
-  |match_ptr1: forall b ofs1 ofs2,
+  |match_ptr_pc: forall b ofs1 ofs2,
       match_pc b ofs1 ofs2 -> match_value (Vptr b (Ptrofs.repr ofs1)) (Vptr b (Ptrofs.repr ofs2))
-  |match_ptr2: forall b o, is_stack b -> match_value (Vptr b o) (Vptr b o)
+  |match_ptr_gdef: forall id, match_value (Vptr (Global id) Ptrofs.zero) (Vptr (Global id) Ptrofs.zero)
+  |match_ptr_s : forall b o, is_stack b -> match_value (Vptr b o) (Vptr b o)
   |match_undef: match_value Vundef Vundef.
 
-(* Mem.iff memval_inject *)
+Inductive match_memval : memval -> memval -> Prop :=
+  match_memval_byte : forall n : byte, match_memval (Byte n) (Byte n)
+| match_memval_frag : forall (v1 v2 : val) (q: quantity) (n:nat),
+                      match_value v1 v2 ->
+                      match_memval (Fragment v1 q n) (Fragment v2 q n)
+| match_memval_undef : match_memval Undef Undef.
+
+(* Mem.iff Mem.mem_inj inject *)
 Inductive match_mem : mem -> mem -> Prop :=
-  match_mem_gen: forall m1 m2,
+  |match_mem_gen: forall m1 m2,
     Mem.support m1 = Mem.support m2 ->
     Mem.mem_access m1 = Mem.mem_access m2 ->
     (*mem_content*)
+    (forall (b:block) (ofs:Z) , match_memval
+                (ZMap.get ofs (NMap.get _ b (Mem.mem_contents m1)))
+                (ZMap.get ofs (NMap.get _ b (Mem.mem_contents m2)))
+    ) ->
     match_mem m1 m2.
 
 Inductive match_states: state -> state -> Prop :=
@@ -78,170 +132,67 @@ Inductive match_states: state -> state -> Prop :=
 Lemma step_simulation:
   forall S1 t S2, step instr_size_1 ge S1 t S2 ->
   forall S1', match_states S1 S1' ->
-  exists S2', step instr_size_2 tge S1' t S2' /\ match_states S2 S2'.
+  exists S2', step instr_size_2 ge S1' t S2' /\ match_states S2 S2'.
 Proof.
   intros. inv H; inv H0.
+  - admit.
+    (*
+      econstructor; eauto. split.
+      econstructor; eauto.                                                                                *)
   - econstructor; eauto. split.
-    econstructor; eauto.
-    
-    econstructor; eauto.
+    rewrite H1 in H8. inv H8.
+    econstructor. eauto. eauto.
+    rewrite Ptrofs.unsigned_repr in H3. rewrite Ptrofs.unsigned_repr.
+    eapply transf_find_instr; eauto. inv H0. rewrite H2 in H. inv H. auto.
+    admit. admit. (* wf? codesize < maxunsigned*)
+    admit. (* eval_builtin *)
+    admit. (* external_call *)
+    admit.
+    admit. (*wrong*)
+    admit. (*wrong*)
+    admit.
   - econstructor; eauto. split.
-    eapply exec_Iop; eauto. rewrite <- genv_eq.
-    eapply eval_operation_memiff; eauto.
-    econstructor; eauto.
-  - econstructor; eauto. split.
-    rewrite <- genv_eq.
-    eapply exec_Iload; eauto.
-    erewrite <- Mem.loadv_iff; eauto.
-    econstructor; eauto.
-  - exploit Mem.storev_iff; eauto.
-    intros (m2' & H3' & IFF').
-    econstructor; eauto. split.
-    rewrite <- genv_eq.
-    eapply exec_Istore; eauto.
-    econstructor; eauto.
-    rewrite <- (Mem.support_storev _ _ _ _ _ H3).
-    rewrite <- (Mem.support_storev _ _ _ _ _ H3').
-    auto.
-  - econstructor; eauto. split.
-    rewrite <- genv_eq.
-    eapply exec_Icall; eauto.
-    econstructor; eauto.
-    eapply Mem.push_stage_left_iff; eauto.
-    simpl. reflexivity.
-  - edestruct Mem.free_parallel_iff as [m1' []]; eauto.
-    inv H14. erewrite <- Mem.astack_return_frame in H7; eauto.
-    erewrite Mem.support_free in H7; eauto. congruence.
-    edestruct Mem.return_frame_iff as [m'2 []]; eauto.
-    assert ({m'3:mem|Mem.pop_stage m'2= Some m'3}).
-      apply Mem.nonempty_pop_stage.
-      erewrite <- Mem.astack_return_frame. 2: eauto.
-      erewrite Mem.support_free. 2: eauto. congruence.
-    destruct X as [m'3 POP].
-    apply Mem.astack_pop_stage in POP as STKm2'.
-    destruct STKm2' as [hd STKm2'].
-    econstructor; eauto. split.
-    rewrite <- genv_eq.
-    eapply exec_Itailcall; eauto.
-    econstructor; eauto.
-    eapply Mem.pop_stage_right_iff; eauto.
-    erewrite <- Mem.astack_return_frame. 2: eauto.
-    erewrite Mem.support_free. 2: eauto. eauto.
-    erewrite <- Mem.support_free in H8; eauto.
-    erewrite Mem.astack_return_frame in H8; eauto.
-    congruence.
-  - exploit Mem.push_stage_left_iff; eauto. intro.
-    exploit external_call_mem_iff; eauto. intros (m'1 & EXT &IFF').
-    econstructor; eauto. split.
-    rewrite <- genv_eq.
-    econstructor; eauto.
-    eapply eval_builtin_args_memiff; eauto.
-    econstructor; eauto.
-    eapply Mem.pop_stage_left_iff; eauto.
-    rewrite <- (external_call_astack _ _ _ _ _ _ _ EXT).
-    apply Mem.astack_pop_stage in H4. destruct H4.
-    rewrite <- (external_call_astack _ _ _ _ _ _ _ H3) in H0.
-    simpl in H0. inv H0. auto.
-  - econstructor; eauto. split.
-    rewrite <- genv_eq.
-    eapply exec_Icond; eauto.
-    erewrite <- eval_condition_memiff; eauto.
-    econstructor; eauto.
-  - econstructor; eauto. split.
-    rewrite <- genv_eq.
-    eapply exec_Ijumptable; eauto.
-    econstructor; eauto.
-  - edestruct Mem.free_parallel_iff as [m'1[]]; eauto.
-    edestruct Mem.return_frame_iff as [m'2[]]; eauto.
-    apply Mem.support_free in H2 as SFREE.
-    apply Mem.support_free in H as SFREE'.
-    apply Mem.astack_return_frame in H3 as ARET.
-    apply Mem.astack_return_frame in H5 as ARET'.
-    inv H12. congruence.
-    assert ({m'3:mem|Mem.pop_stage m'2= Some m'3}).
-      apply Mem.nonempty_pop_stage. congruence.
-    destruct X as [m'3 POP].
-    econstructor; eauto. split.
-    eapply exec_Ireturn; eauto.
-    econstructor; eauto.
-    eapply Mem.pop_stage_right_iff; eauto.
-    rewrite <- ARET. rewrite SFREE. eauto.
-    apply Mem.astack_pop_stage in POP. destruct POP. congruence.
-  - edestruct Mem.alloc_frame_iff as [m'1 []]; eauto.
-    edestruct Mem.alloc_parallel_iff as [m'2 []]; eauto.
-    apply Mem.astack_alloc_frame in H1 as SAF.
-    apply Mem.astack_alloc_frame in H as SAF'.
-    apply Mem.support_alloc in H2 as SAL.
-    apply Mem.support_alloc in H4 as SAL'.
-    exploit Mem.push_stage_right_iff. apply H5. intro.
-    apply Mem.astack_record_frame in H3 as ASTK.
-    apply Mem.record_frame_size1 in H3 as SIZE. simpl in SIZE.
-    destruct ASTK as (hd' & tl' & ASTKm' & ASTKm''). rewrite ASTKm' in SIZE.
-    rewrite SAL in ASTKm'.
-    unfold sup_incr in ASTKm'. destr_in ASTKm'. simpl in ASTKm'.
-    rewrite <- SAF in ASTKm'.
-    rewrite H10 in ASTKm'. inv ASTKm'.
-    assert ({m'3:mem|Mem.record_frame (Mem.push_stage m'2) (Memory.mk_frame (fn_stack_requirements id)) = Some m'3}).
-      apply Mem.request_record_frame. simpl. congruence.
-      simpl. rewrite SAL'. unfold sup_incr. destr. simpl.
-      rewrite <- SAF'.
-      apply match_stackadt_size in H11. simpl in *.
-      generalize (size_of_all_frames_pos hd'). lia.
-    destruct X as [m'3 RECORD].
-    econstructor; eauto. split.
-    econstructor; eauto.
-    econstructor; eauto.
-    eapply Mem.record_frame_safe_iff; eauto.
-    apply Mem.astack_record_frame in RECORD as ASTK.
-    destruct ASTK as (hd''&tl''&STKm'1&STKm'2).
-    rewrite STKm'2. simpl in STKm'1. inv STKm'1.
-    rewrite ASTKm''. econstructor. 2:eauto.
-    rewrite SAL'. unfold sup_incr. destr. simpl. rewrite <- SAF'. auto.
-  - exploit external_call_mem_iff; eauto.
-    intros (m2' & H1'& IFF).
-    econstructor; eauto. split.
-    rewrite <- genv_eq.
-    econstructor; eauto.
-    econstructor; eauto.
-    rewrite <- (external_call_astack _ _ _ _ _ _ _ H1). eauto.
-    rewrite <- (external_call_astack _ _ _ _ _ _ _ H1'). eauto.
-  - apply Mem.stack_pop_stage in H1 as H1'. destruct H1'.
-    econstructor; eauto. split.
-    econstructor; eauto.
-    econstructor; eauto.
-    eapply Mem.pop_stage_left_iff; eauto.
-    apply Mem.astack_pop_stage in H1. destruct H1. rewrite H in H6. inv H6.
-    auto.
-Qed.
+    eapply exec_step_external.
+    rewrite H1 in H7. inv H7. inv H0.
+    congruence. auto. auto. eauto.
+    rewrite <- H6. admit.
+    congruence. congruence.
+    rewrite <- H6. congruence. congruence. congruence.
+    admit. rewrite <- H6. congruence. congruence. congruence.
+    admit. admit. reflexivity.
+    admit.
+
+Admitted.
 
 Lemma transf_initial_states:
-  forall S1, RTL.initial_state prog S1 ->
-  exists S2, initial_state tprog S2 /\ match_states S1 S2.
+  forall S1, initial_state prog S1 ->
+  exists S2, initial_state prog S2 /\ match_states S1 S2.
 Proof.
-  intros. inv TRANSL. inv H.
-  exists (Callstate nil f nil m1 (prog_main tprog)).
-  split.
+  intros. exists S1. split. auto. inv H.
   econstructor; eauto.
-  econstructor; eauto.
-  eapply Mem.push_stage_left_iff. apply Mem.iff_refl.
-  simpl. reflexivity. erewrite Mem.astack_alloc; eauto.
-  apply Genv.init_mem_astack in H0. rewrite H0.
-  constructor.
-Qed.
+  unfold rs0. real_simpl_regs.
+  apply Genv.genv_vars_eq in H3. rewrite H3.
+  eapply match_ptr_gdef.
+  unfold rs0. real_simpl_regs. constructor.
+  admit. (* init_mem_match *)
+Admitted.
 
 Lemma transf_final_states:
-  forall S1 S2 r, match_states S1 S2 -> RTL.final_state S1 r -> final_state S2 r.
+  forall S1 S2 r, match_states S1 S2 -> final_state S1 r -> final_state S2 r.
 Proof.
   intros. inv H0. inv H. constructor.
+  rewrite H1 in H5. unfold Vnullptr in *.
+  destruct (Archi.ptr64); inv H5; eauto.
+  rewrite <- H4. auto. congruence. congruence.
 Qed.
 
 
 Theorem transf_program_correct:
-  forward_simulation (RTL.semantics fn_stack_requirements prog)
-                     (RTLmach.semantics fn_stack_requirements tprog).
+  forward_simulation (semantics instr_size_1 prog)
+                     (semantics instr_size_2 prog).
 Proof.
   eapply forward_simulation_step.
-  rewrite prog_eq. auto.
+  reflexivity.
   eexact transf_initial_states.
   eexact transf_final_states.
   exact step_simulation.
@@ -249,8 +200,3 @@ Qed.
 
 End PRESERVATION.
 
-Instance TransfRTLmachLink : TransfLink match_prog.
-Proof.
-  red; intros. destruct (link_linkorder _ _ _ H) as (LO1 & LO2).
-  eexists p. split. inv H0. inv H1. auto. reflexivity.
-Qed.
